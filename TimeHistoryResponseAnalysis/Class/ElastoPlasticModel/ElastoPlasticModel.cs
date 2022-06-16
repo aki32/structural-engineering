4552e790fd9da1f492a8b84f0583880799c11fa8
aki32 <aki32.png@gmail.com>
2022-06-17 02:53:50 +0900

    organized

--- TimeHistoryResponseAnalysis/Class/ElastoPlasticModel/ElastoPlasticModel.cs
+++ TimeHistoryResponseAnalysis/Class/ElastoPlasticModel/ElastoPlasticModel.cs
@@ -9,7 +9,6 @@ namespace TimeHistoryResponseAnalysis.Class.ElastoPlasticModel
     internal interface IElastoPlasticModel
     {
         public double K1 { get; set; }
-
         internal double CalcNextF(double targetX);
     }
 }
