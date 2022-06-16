64651a77cf437375ab3a6db7bc425003422fb4cd
aki32 <aki32.png@gmail.com>
2022-06-16 23:53:46 +0900

    organized DrawTripertiteFromCsv project

--- DrawTripertiteFromCsv/DrawTripertiteFromCsv.py
+++ DrawTripertiteFromCsv/DrawTripertiteFromCsv.py
@@ -1,6 +1,6 @@
 # -*- coding:utf8 -*-
 import json, os
-from msilib import Directory
+from pathlib import Path
 import pandas as pd
 import matplotlib.pyplot as plt
 from Class.SpectrumInfo import SpectrumInfo
@@ -8,7 +8,6 @@ from Class.Graph import Graph
 
 
 ### Initialize
-
 BASE_PATH = '# TestModel'
 
 OUTPUT_DIR_PATH = f'{BASE_PATH}/output'
@@ -20,23 +19,21 @@ INPUT_CONFIG_FILE_NAMES = [
 ]
 
 
-### Draw Tripertite
-
-for configName in INPUT_CONFIG_FILE_NAMES:
+### Define
+def DrawTripertite(configFilePath, spectrumFilePath, saveImagePath):
     
     print(configName)
     print("  Started")
     
     ## Read config
-    with open(f'{INPUT_CONFIG_DIR_PATH}/{configName}.json') as f:
+    with open(configFilePath) as f:
         config = json.load(f)
         graph = Graph(config['graph'])
         specInfoList = [SpectrumInfo(spectrumDict) for spectrumDict in config['spectra']]
 
-
     ## Draw Tripertite
     for specInfo in specInfoList:
-        df = pd.read_csv(INPUT_SPECTRUM_FILE_PATH)
+        df = pd.read_csv(spectrumFilePath)
         X = df[df.columns[0]].values
         Y = df[specInfo.Name].values
         graph.DrawVelSpectrumLine(X, Y, specInfo.Name, specInfo.Color, specInfo.LineStyle)
@@ -45,15 +42,20 @@ for configName in INPUT_CONFIG_FILE_NAMES:
     graph.Format()
     
     ## Save and show fig
-    if not os.path.exists(OUTPUT_DIR_PATH):
-        os.makedirs(OUTPUT_DIR_PATH)
-    plt.savefig(f'{OUTPUT_DIR_PATH}/result {configName}.png', dpi=graph.Dpi)
-    #plt.show()
-
-    print("  Finished")
-
-print("All Finished")
+    outputDir=Path(saveImagePath)
+    if not os.path.exists(outputDir.parent):
+        os.makedirs(outputDir.parent)
+    plt.savefig(saveImagePath, dpi=graph.Dpi)
+    print("  Done")
 
 
+### Run
+for configName in INPUT_CONFIG_FILE_NAMES:
+    DrawTripertite(
+        f'{INPUT_CONFIG_DIR_PATH}/{configName}.json',
+        INPUT_SPECTRUM_FILE_PATH,
+        f'{OUTPUT_DIR_PATH}/result {configName}.png')
+
+print("All Done !")
 
 
