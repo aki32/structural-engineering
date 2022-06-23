b3b99c08d41f6a39a0cace188cb06162fe77e77e
aki32 <aki32.png@gmail.com>
2022-06-23 14:14:46 +0900

    added DegradingCloughModel

--- TimeHistoryResponseAnalysis/Class/RestoringForceCharacteristics/33 DegradingCloughModel.cs
+++ TimeHistoryResponseAnalysis/Class/RestoringForceCharacteristics/33 DegradingCloughModel.cs
@@ -8,20 +8,31 @@ public class DegradingCloughModel : RestoringForceCharacteristics
     public double beta { get; set; }
     public double Fy { get; set; }
     public double alpha { get; set; }
-    public double K2 => K1 * beta;
 
+    public double K2 => K1 * beta;
+    public double Kr => K1 * Math.Pow(Math.Abs(Math.Max(1, DegradeStartX / Xy)), -alpha);
+
+    private double Xy = 0d;
     private double MaxF = 0d;
     private double MaxX = 0d;
     private double MinF = 0d;
     private double MinX = 0d;
 
     private bool IsInDegradingState = false;
-    private bool IsDegradingStatePositive = false;
-    private double Xr_Start = 0;
-    private double Kr = 0;
+    private double DegradeStartX = 0;
+    private double DegradeStartF = 0;
 
     #endregion
 
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="K1"></param>
+    /// <param name="beta"></param>
+    /// <param name="Fy"></param>
+    /// <param name="alpha">
+    /// RC : commonly, 0.4 - 0.5
+    /// </param>
     public DegradingCloughModel(double K1, double beta, double Fy, double alpha)
     {
         this.K1 = K1;
@@ -29,12 +40,18 @@ public class DegradingCloughModel : RestoringForceCharacteristics
         this.Fy = Fy;
         this.alpha = alpha;
 
+        Xy = Fy;
         MaxF = Fy;
-        MaxX = MaxF / K1;
+        MaxX = Fy / K1;
         MinF = -Fy;
-        MinX = MinF / K1;
+        MinX = -Fy / K1;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="targetX"></param>
+    /// <returns></returns>
     public override double CalcNextF(double targetX)
     {
         if (LastX == targetX)
@@ -44,40 +61,68 @@ public class DegradingCloughModel : RestoringForceCharacteristics
         LastF = CurrentF;
         CurrentX = targetX;
 
-      
-
         #region fを求める
 
         // 設計イラストの通り
         var dX = CurrentX - LastX;
         var f1 = K1 * dX + LastF;
+        var fr = Kr * dX + LastF;
         var fy = K2 * CurrentX + ((CurrentX > LastX) ? Fy : -Fy);
-        double fc; // fc0と兼用
+        double fc; // fcrと兼用
 
         // 向かってる先でX軸をまたがない／またぐ
         if ((CurrentX > LastX && LastF > 0) || (CurrentX < LastX && LastF < 0))
         {
-            if (CurrentX > LastX)
-                fc = GetF(LastX, LastF, MaxX, MaxF, CurrentX);
+            // もし DegradingState に居るのにX軸をまたがないなら，fcをセットバックさせる。
+            if (IsInDegradingState)
+            {
+                // fcr
+                if (CurrentX > LastX)
+                    fc = CalcF_FromPoints(DegradeStartX, DegradeStartF, MaxX, MaxF, CurrentX);
+                else
+                    fc = CalcF_FromPoints(DegradeStartX, DegradeStartF, MinX, MinF, CurrentX);
+            }
             else
-                fc = GetF(LastX, LastF, MinX, MinF, CurrentX);
+            {
+                // fc
+                if (CurrentX > LastX)
+                    fc = CalcF_FromPoints(LastX, LastF, MaxX, MaxF, CurrentX);
+                else
+                    fc = CalcF_FromPoints(LastX, LastF, MinX, MinF, CurrentX);
+            }
         }
         else
         {
+            // fc0
             var HitX = LastX + (-LastF / K1);
 
             if (CurrentX > LastX)
-                fc = GetF(HitX, 0, MaxX, MaxF, CurrentX);
+                fc = CalcF_FromPoints(HitX, 0, MaxX, MaxF, CurrentX);
             else
-                fc = GetF(HitX, 0, MinX, MinF, CurrentX);
+                fc = CalcF_FromPoints(HitX, 0, MinX, MinF, CurrentX);
         }
 
         // 最小値／最大値
-        var fs = new List<double> { f1, fy, fc };
+        var fs = new List<double> { fr, fy, fc };
         if (CurrentX > LastX)
             CurrentF = fs.Min();
         else
             CurrentF = fs.Max();
+
+        // もし fr を採用した場合は，DegradingState に居ることになる。そうでなければ DegradingState 外
+        if (fr == CurrentF)
+        {
+            if (IsInDegradingState == false)
+            {
+                DegradeStartX = CurrentX;
+                DegradeStartF = CurrentF;
+                IsInDegradingState = true;
+            }
+        }
+        else
+        {
+            IsInDegradingState = false;
+        }
 
         #endregion
 
@@ -85,6 +130,29 @@ public class DegradingCloughModel : RestoringForceCharacteristics
         return CurrentF;
     }
 
+    /// <summary>
+    /// (X1,F1),(X2,F2)の2点を通過する直線上の targetX での F を返す。
+    /// </summary>
+    /// <param name="X1"></param>
+    /// <param name="F1"></param>
+    /// <param name="X2"></param>
+    /// <param name="Y2"></param>
+    /// <param name="targetX"></param>
+    /// <returns></returns>
+    private double CalcF_FromPoints(double X1, double F1, double X2, double Y2, double targetX)
+    {
+        double Kc;
+        if (X1 == X2)
+            Kc = K2;
+        else
+            Kc = (Y2 - F1) / (X2 - X1);
+        Kc = Math.Min(K1, Kc); // for safety
+        return Kc * (targetX - X1) + F1;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
     private void UpdateMinMax()
     {
         if (CurrentX > MaxX)
@@ -97,26 +165,6 @@ public class DegradingCloughModel : RestoringForceCharacteristics
             MinX = CurrentX;
             MinF = CurrentF;
         }
-    }
-
-    /// <summary>
-    /// (X1,F1),(X2,F2)の2点を通過する直線上の targetX での F を返す。
-    /// </summary>
-    /// <param name="X1"></param>
-    /// <param name="F1"></param>
-    /// <param name="X2"></param>
-    /// <param name="Y2"></param>
-    /// <param name="targetX"></param>
-    /// <returns></returns>
-    double GetF(double X1, double F1, double X2, double Y2, double targetX)
-    {
-        double Kc;
-        if (X1 == X2)
-            Kc = K2;
-        else
-            Kc = (Y2 - F1) / (X2 - X1);
-        Kc = Math.Min(K1, Kc); // for safety
-        return Kc * (targetX - X1) + F1;
     }
 
 }
--- TimeHistoryResponseAnalysis/Program.cs
+++ TimeHistoryResponseAnalysis/Program.cs
@@ -54,38 +54,38 @@ namespace TimeHistoryResponseAnalysis
 
                 // rfc test
                 {
-                    //var rfc = new CloughModel(2, 0.1, 90);
-                    var rfc = new DegradingCloughModel(2, 0.1, 8, 0.4);
+                    ////var rfc = new CloughModel(2, 0.1, 90);
+                    //var rfc = new DegradingCloughModel(2, 0.1, 90, 0.4);
 
-                    var tester = new RFCTester(rfc);
-                    var wave = RFCTester.GetTestWave1();
-                    var result = tester.Calc(wave);
+                    //var tester = new RFCTester(rfc);
+                    //var wave = RFCTester.GetTestWave1();
+                    //var result = tester.Calc(wave);
 
-                    var saveFile = new FileInfo(@$"{basePath}\output\result - test.csv");
-                    result.OutputTimeHistoryToCsv(saveFile);
+                    //var saveFile = new FileInfo(@$"{basePath}\output\result - test.csv");
+                    //result.OutputTimeHistoryToCsv(saveFile);
                 }
 
-                //// combined
-                //{
-                //    //var rfc = new ElasticModel(2);
-                //    //var rfc = new PerfectElastoPlasticModel(2, 8);
-                //    //var rfc = new BilinearModel(2, 0.1, 8);
-                //    //var rfc = new DegradingBilinearModel(2, 0.1, 8, 0.4);
-                //    //var rfc = new CloughModel(2, 0.1, 8);
-                //    var rfc = new DegradingCloughModel(2, 0.1, 8, 0.4);
+                // combined
+                {
+                    //var rfc = new ElasticModel(2);
+                    //var rfc = new PerfectElastoPlasticModel(2, 8);
+                    //var rfc = new BilinearModel(2, 0.1, 8);
+                    //var rfc = new DegradingBilinearModel(2, 0.1, 8, 0.4);
+                    //var rfc = new CloughModel(2, 0.1, 8);
+                    var rfc = new DegradingCloughModel(2, 0.1, 8, 0.4);
 
-                //    var model = SDoFModel.FromT(1, 0.03, rfc);
+                    var model = SDoFModel.FromT(1, 0.03, rfc);
 
-                //    var waveCsv = new FileInfo(@$"{basePath}\Hachinohe-NS.csv");
-                //    var wave = TimeHistory.FromCsv(waveCsv, new string[] { "t", "ytt" });
+                    var waveCsv = new FileInfo(@$"{basePath}\Hachinohe-NS.csv");
+                    var wave = TimeHistory.FromCsv(waveCsv, new string[] { "t", "ytt" });
 
-                //    //var waveAnalysisModel = new NewmarkBetaModel(0.25);
-                //    var waveAnalysisModel = new NigamJenningsModel();
+                    //var waveAnalysisModel = new NewmarkBetaModel(0.25);
+                    var waveAnalysisModel = new NigamJenningsModel();
 
-                //    var result = model.Calc(wave, waveAnalysisModel);
+                    var result = model.Calc(wave, waveAnalysisModel);
 
-                //    result.OutputTimeHistoryToCsv();
-                //}
+                    result.OutputTimeHistoryToCsv();
+                }
 
 
                 // spectrum analysis with newmark beta
